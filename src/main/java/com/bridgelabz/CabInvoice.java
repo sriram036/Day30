de62a2755d83e63f43e29dcb93d4f842a669f469
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-27 17:48:58 +0530

    Added: calculate fare

--- src/main/java/com/bridgelabz/CabInvoice.java
+++ src/main/java/com/bridgelabz/CabInvoice.java
@@ -1,4 +1,9 @@
 package com.bridgelabz;
 
 public class CabInvoice {
+    public static final int FARE_PER_KM = 10;
+    public static final int FARE_PER_MINUTE = 1;
+    public double calculateFare(int distance, int time) {
+        return FARE_PER_KM * distance + FARE_PER_MINUTE * time;
+    }
 }
