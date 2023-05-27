1712d3a9e9ea30a6e3b4a72750f161b74ddde370
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-05-27 18:23:07 +0530

    Added: multiple rides

---
+++ src/main/java/com/bridgelabz/Ride.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Ride {
+    int distance;
+    int time;
+    public Ride(int distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
